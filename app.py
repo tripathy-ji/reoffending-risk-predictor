8bb4a9c808a22e4c2af1ebb57b8e70aafa00b115
tripathy-ji <ayushwastemail@gmail.com>
2025-06-15 13:41:57 +0530

    Fix server start for Render

--- app.py
+++ app.py
@@ -40,5 +40,9 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
